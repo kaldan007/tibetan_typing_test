1d9a20f17a2d075463f385d6015fc4615763a46e
kaldan007 <kaldantenzin@gmail.com>
2023-06-01 14:41:12 +0530

    timer updated

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
 const quoteSection = document.getElementById("quote");
 const userInput = document.getElementById("quote-input");
 let quote = "";
-let time = 60;
+let time = 360;
 let timer = "";
 let mistakes = 0;
 
